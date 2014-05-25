e88f08b782efb6a3cdb853e6bcf2a7a34ceda098
hrk fkm <fkm@fkmsoft.jp>
2014-05-26 00:44:09 +0900

    Add GeoBox and GeoDistance API

--- java/src/main/java/jp/fkmsoft/libs/kiilib/entities/KiiClause.java
+++ java/src/main/java/jp/fkmsoft/libs/kiilib/entities/KiiClause.java
@@ -46,7 +46,56 @@ public class KiiClause {
         }
         
         return clause;
-    }    
+    }
+
+    public static KiiClause geoBox(String field, double nwLat, double nwLon, double seLat, double seLon) {
+        KiiClause clause = new KiiClause(TYPE_GEO_BOX);
+        try {
+            clause.json.put(FIELD_FIELD, field);
+            clause.json.put(FIELD_BOX, createBox(createPoint(nwLat, nwLon), createPoint(seLat, seLon)));
+        } catch (JSONException e) {
+            // nop
+        }
+
+        return clause;
+    }
+
+    public static KiiClause geoDistance(String field, double centerLat, double centerLon, double radius) {
+        KiiClause clause = new KiiClause(TYPE_GEO_DISTANCE);
+        try {
+            clause.json.put(FIELD_FIELD, field);
+            clause.json.put(FIELD_CENTER, createPoint(centerLat, centerLon));
+            clause.json.put(FIELD_RADIUS, radius);
+        } catch (JSONException e) {
+            // nop
+        }
+
+        return clause;
+    }
+
+    private static JSONObject createBox(JSONObject northWest, JSONObject southEast) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put(FIELD_NORTH_WEST, northWest);
+            json.put(FIELD_SOUTH_EAST, southEast);
+        } catch (JSONException e) {
+            // nop
+        }
+
+        return json;
+    }
+
+    private static JSONObject createPoint(double latitude, double longitude) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("_type", "point");
+            json.put(FIELD_LATITUDE, latitude);
+            json.put(FIELD_LONGITUDE, longitude);
+        } catch (JSONException e) {
+            // nop
+        }
+        return json;
+    }
     
     public static KiiClause and(KiiClause... clauses) {
         KiiClause clause = new KiiClause(TYPE_AND);
@@ -106,6 +155,13 @@ public class KiiClause {
     private static final String FIELD_LOWER_INCLUDED = "lowerIncluded";
     private static final String FIELD_UPPER_LIMIT = "upperLimit";
     private static final String FIELD_UPPER_INCLUDED = "upperIncluded";
+    private static final String FIELD_BOX = "box";
+    private static final String FIELD_NORTH_WEST = "nw";
+    private static final String FIELD_SOUTH_EAST = "se";
+    private static final String FIELD_CENTER = "center";
+    private static final String FIELD_RADIUS = "radius";
+    private static final String FIELD_LATITUDE = "lat";
+    private static final String FIELD_LONGITUDE = "lon";
     private static final String FIELD_CLAUSES = "clauses";
     
     private static final String TYPE_ALL = "all";
@@ -114,6 +170,8 @@ public class KiiClause {
     private static final String TYPE_OR = "or";
     private static final String TYPE_IN = "in";
     private static final String TYPE_RANGE = "range";
+    private static final String TYPE_GEO_BOX = "geobox";
+    private static final String TYPE_GEO_DISTANCE = "geodistance";
     
 
     private final JSONObject json = new JSONObject();
